d91ae8871da4374b884dc3dbffe717b44d77c3f3
gurnoork-k <152222637+gurnoork-k@users.noreply.github.com>
2026-09-16 22:23:22 +0530

    ✅ Maximum Subarray | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 71.7 MB | 2026-09-16

---
+++ leetcode/medium/0053-maximum-subarray/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    /*brute force --------------------------
+    int maxSubArray(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int sum = 0;
+
+        for(int i = 0; i< nums.size(); i++){
+            for(int j = i; j< nums.size(); j++){
+                sum += nums[j];
+                maxi = max(sum , maxi);
+            }
+        }
+        return maxi;
+    }*/
+
+    int maxSubArray(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int sum = 0;
+
+        for(int i = 0; i< nums.size(); i++){
+            sum += nums[i];
+            maxi = max(sum , maxi);
+
+            if(sum < 0) sum = 0;
+        }
+        return maxi;
+    }
+};
