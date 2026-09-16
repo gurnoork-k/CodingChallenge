dfbe129484ce6ad83bce2df5d26eccd3723e963d
gurnoork-k <152222637+gurnoork-k@users.noreply.github.com>
2026-09-16 23:06:21 +0530

    ✅ Right View of Binary Tree | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ gfg/easy/right-view-of-binary-tree/solution.cpp
@@ -0,0 +1,39 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+private: 
+    void solve(Node* root, vector<int> &ans, int level){
+        if(root == NULL){
+            return;
+        }
+
+        if(level == ans.size()){
+            ans.push_back(root-> data);
+        }
+        solve(root-> right, ans, level+1);
+        solve(root-> left, ans, level+1);
+        
+    }
+    
+  public:
+    vector<int> rightView(Node *root) {
+        //  code here
+        vector<int> ans;
+        solve(root, ans, 0);
+        return ans;
+    }
+
+};
