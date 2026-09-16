16e85dd2fd71542f532b03d69ea5c624a324ced6
gurnoork-k <152222637+gurnoork-k@users.noreply.github.com>
2026-09-16 23:03:23 +0530

    ✅ Left View of Binary Tree | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ gfg/easy/left-view-of-binary-tree/solution.cpp
@@ -0,0 +1,34 @@
+/* Structure of Binary Tree Node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+
+class Solution {
+private:
+    void solve(Node* root, vector<int> &ans, int level){
+        if(root == NULL){
+            return;
+        }
+        
+        if(level == ans.size()){
+            ans.push_back(root-> data);
+        }
+        solve(root-> left, ans, level+1);
+        solve(root-> right, ans, level+1);
+    }
+  public:
+    vector<int> leftView(Node *root) {
+        // code here
+        vector<int> ans;
+        solve(root, ans, 0);
+        return ans;
+    }
+};
