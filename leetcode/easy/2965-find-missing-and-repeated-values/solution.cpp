7557103aa27f0682b54ae6e9d078489d078d06c5
gurnoork-k <152222637+gurnoork-k@users.noreply.github.com>
2026-09-22 03:19:37 +0530

    ✅ Find Missing and Repeated Values | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 27.4 MB | 2026-09-21

--- leetcode/easy/2965-find-missing-and-repeated-values/solution.cpp
+++ leetcode/easy/2965-find-missing-and-repeated-values/solution.cpp
@@ -1,31 +1,53 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int> ans;
-        map<int, int> count;
-        int r = grid.size();
-        int c = grid[0].size();
+        // vector<int> ans;
+        // map<int, int> count;
+        // int r = grid.size();
+        // int c = grid[0].size();
 
-        for(int i= 0; i< c; i++){
-            for(int j= 0; j< c; j++){
-                if(count.find(grid[i][j]) == count.end()){
-                    count[ grid[i][j] ] = 1;
-                }
-                else count[ grid[i][j] ]+= 1;
-            }
+        // for(int i= 0; i< c; i++){
+        //     for(int j= 0; j< c; j++){
+        //         if(count.find(grid[i][j]) == count.end()){
+        //             count[ grid[i][j] ] = 1;
+        //         }   
+        //         else count[ grid[i][j] ]+= 1;
+        //     }
             
+        // }
+
+        // for(int i=1; i <= c*r; i++){
+        //     if(count[i] > 1){
+        //         ans.push_back(i);
+        //     }
+        // }
+        // for(int i=1; i <= c*r; i++){
+        //     if(count[i] == 0){
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;
+
+        int n = grid.size();
+        int total = n*n;
+        vector<int> count(total+1);
+
+        for(auto row: grid){
+            for(auto x: row){
+                count[x]++;
+            }
         }
 
-        for(int i=1; i <= c*r; i++){
-            if(count[i] > 1){
-                ans.push_back(i);
+        int rep = -1;
+        int mis = -1;
+        for(int i = 1; i <= total; i++){
+            if(count[i] == 2){
+                rep = i;
+            }
+            else if(count[i] == 0){
+                mis = i;
             }
         }
-        for(int i=1; i <= c*r; i++){
-            if(count[i] == 0){
-                ans.push_back(i);
-            }
-        }
-        return ans;
+        return {rep, mis};
     }
 };
